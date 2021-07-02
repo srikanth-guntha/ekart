1694cb4e93b50ab8c79fc653984c081c4cf8c5a2
Srikanth Guntha <sguntha@pkrmsezdc.com>
2021-07-02 14:24:08 +0530

    committed heroku deployment changes

--- apps/express-app/src/main.ts
+++ apps/express-app/src/main.ts
@@ -1,9 +1,14 @@
 import * as express from 'express';
-
+import * as path from 'path';
+console.log(__dirname);
+const CLIENT_BUILD_PATH = path.join(__dirname, '../ekart');
 const app = express();
 import { router } from './routes/bookapi';
 app.use('/api', router);
-
+app.use(express.static(CLIENT_BUILD_PATH));
+app.get('*', (request, response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+});
 const port = process.env.port || 4444;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
@@ -11,5 +16,3 @@ const server = app.listen(port, () => {
 server.on('error', console.error);
 
 module.exports = app;
-
-
